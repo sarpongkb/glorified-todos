ec0849f60508e38970dc9ecf59c8555caee88b2c
Boadu <sarpongkb@gmail.com>
2022-02-15 18:25:24 -0800

    Add sb-todo

---
+++ sb-todo/src/main/java/com/sarpongkb/sbtodo/SbTodoApplication.java
@@ -0,0 +1,13 @@
+package com.sarpongkb.sbtodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbTodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbTodoApplication.class, args);
+	}
+
+}
---
+++ sb-todo/src/test/java/com/sarpongkb/sbtodo/SbTodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sarpongkb.sbtodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbTodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
